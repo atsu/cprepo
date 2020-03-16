5bee6a7ffa9d2010d6ad5561d8b515bab9a66ff7
TQ Berg <tq@atsu.io>
2020-03-16 16:14:34 -0700

    Iterating

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/sh -l
 
-echo "-> $0"
-echo "src -> $1"
-echo "dst -> $2"
+set -x
+
+if diff -c "$1" "$2"
+then
+  echo "same/success"
+else
+  echo "diff/failure"
+  #git config --local user.email "action@github.com"
+  #git config --local user.name "GitHub cprepo action"
+  #git -c core.quotepath=false checkout -b repofilesync/foo
+  cp "$1" "$2"
+  #git add .
+  #git commit -m "update workflow"
+  ls -al "$2"
+  #git status
+fi
