dfbbeff6015afa1a661e9ca93223b25e1d413b51
TQ Berg <tq@atsu.io>
2020-03-16 17:45:58 -0700

    Cleaning up entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,11 @@
 
 set -x
 
-if diff -U3 "$1" "$2"
+if ! diff -U3 "$1" "$2"
 then
-  echo "same/success"
-else
   echo "exit code = $?"
   echo "diff/failure"
-  #git config --local user.email "action@github.com"
-  #git config --local user.name "GitHub cprepo action"
-  #git -c core.quotepath=false checkout -b repofilesync/foo
   cp "$1" "$2"
-  #git add .
-  #git commit -m "update workflow"
   ls -al "$2"
   cat "$2"
-  #git status
 fi
