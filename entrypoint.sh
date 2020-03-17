f899e2f81f32342b0714fb1a92eace52f058f750
TQ Berg <tq@atsu.io>
2020-03-17 13:14:35 -0700

    Add "changed" output parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,7 @@ then
   echo "source file $1 differs from destination file $2"
   echo "will copy from source path to destination path"
   cp "$1" "$2"
+  echo "::set-output name=changed::yes"
+else
+  echo "::set-output name=changed::no"
 fi
