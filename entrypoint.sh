7cc0399327659354a1902303b52e53fd57b4c335
TQ Berg <tq@atsu.io>
2020-03-18 16:10:08 -0700

    Add output value for use by downstream jobs steps, check exit codes (#2)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,28 @@
 #!/bin/sh -l
 
-if ! diff -U3 "$1" "$2"
+if [ -f "$2" ]
 then
-  echo "source file $1 differs from destination file $2"
-  echo "will copy from source path to destination path"
-  cp "$1" "$2"
+    diff -U3 "$1" "$2"
+    diff_result=$?
+
+    if [ $diff_result -gt 1 ]
+    then
+	      echo "diff failed with exit code = $diff_result"
+        exit 1
+    fi
+
+    if [ $diff_result -eq 0 ]
+    then
+	    echo ::set-output name=changed::no
+	    exit 0
+    fi
+fi
+
+echo "copying from source path = $1 to destination path = $2"
+cp "$1" "$2"
+cp_result=$?
+
+if [ $cp_result -eq 0 ]
+then
+    echo ::set-output name=changed::yes
 fi
